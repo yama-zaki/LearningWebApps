b1ea16d1941caa54a812f301d80454f3b97cfe3a
yama-zaki <yamazaki@it-nasu.com>
2018-08-01 18:29:23 +0900

    テキストエリアに文字を入力した時に残りの文字数をカウントして表示出来るようにしました

--- JavaScript学習/Brackets/sample_js/07/form/practice/js/app.js
+++ JavaScript学習/Brackets/sample_js/07/form/practice/js/app.js
@@ -2,10 +2,26 @@
 // フォームの要素を取得
 var button = document.getElementById('button');
 var form = document.getElementById('form');
+var textarea = document.getElementById('textarea');
+
+// 文字数制限
+var maxTextNum = textarea.getAttribute('maxlength');
+
+// 要素の追加 **********/
+// 残りの文字数を表示する要素の追加
+var textMessage = document.createElement('div');
+var parent = textarea.parentElement;
+parent.insertBefore(textMessage, textarea);
 
 /* イベント処理 *********/
 // お問い合わせボタンを押した時
-button.addEventListener('click', function() {
+document.addEventListener('click', function() {
   // フォームを表示
   form.style.display = 'block';
+});
+
+// テキストエリアでキーをタイプしたとき
+textarea.addEventListener('keyup', function() {
+  var currentTextNum = textarea.value.length;
+  textMessage.innerHTML = '<p>あと「' + (maxTextNum - currentTextNum) + '」文字入力できます。</P>';
 });
