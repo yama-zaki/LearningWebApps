f02a89278961c9564d0e22ce2e18bb66cd691543
yama-zaki <yamazaki@it-nasu.com>
2018-08-01 16:19:20 +0900

    要素のスタイルを変更するについて

--- JavaScript学習/Brackets/sample_js/06/change/practice/js/app.js
+++ JavaScript学習/Brackets/sample_js/06/change/practice/js/app.js
@@ -1,2 +1,5 @@
 var practice = document.getElementById('practice');
 practice.innerHTML = '<h1>れんしゅう</h1>';
+practice.style.backgroundColor = '#999999';
+practice.style.fontSize = '30px';
+practice.style.color = '#FFFFFF';
