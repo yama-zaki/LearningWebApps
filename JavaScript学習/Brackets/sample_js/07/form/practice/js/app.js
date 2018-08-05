31df5fc309c641d35660f6199def629475264b96
yama-zaki <yamazaki@it-nasu.com>
2018-08-05 18:42:19 +0900

    タイマーで残り時間が表示されるようにして、残り時間が０になったら時間切れの警告を表示するようにしました

--- JavaScript学習/Brackets/sample_js/07/form/practice/js/app.js
+++ JavaScript学習/Brackets/sample_js/07/form/practice/js/app.js
@@ -7,19 +7,37 @@ var textarea = document.getElementById('textarea');
 // 文字数制限
 var maxTextNum = textarea.getAttribute('maxlength');
 
+// 残り時間
+var reminingTimeNum = 30;
+
+
 // 要素の追加 **********/
 // 残りの文字数を表示する要素の追加
 var textMessage = document.createElement('div');
 var parent = textarea.parentElement;
 parent.insertBefore(textMessage, textarea);
 
+// 残時間を表示する要素の追加
+var timeMasseage = document.createElement('div');
+parent.insertBefore(timeMasseage, null);
+
 /* イベント処理 *********/
 // お問い合わせボタンを押した時
 document.addEventListener('click', function() {
   // フォームを表示
   form.style.display = 'block';
-});
 
+  // タイマー処理で残り時間を表示
+  var timerID = setInterval(function(){
+    timeMasseage.innerHTML = '<p>制限時間：' +  reminingTimeNum + '秒</p>';
+    if (reminingTimeNum <= 0) {
+      alert('制限時間終了');
+      clearInterval(timerID);
+    }
+    reminingTimeNum--;
+  },1000);
+});   
+    
 // テキストエリアでキーをタイプしたとき
 textarea.addEventListener('keyup', function() {
   var currentTextNum = textarea.value.length;
