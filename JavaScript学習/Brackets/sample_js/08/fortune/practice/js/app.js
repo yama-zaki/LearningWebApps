cff5f50fd5f13b47e52c6f3ea562d5968cc9fd6d
yama-zaki <yamazaki@it-nasu.com>
2018-08-07 16:35:53 +0900

    コンソールではなくブラウザ上に表示されるようにしました

--- JavaScript学習/Brackets/sample_js/08/fortune/practice/js/app.js
+++ JavaScript学習/Brackets/sample_js/08/fortune/practice/js/app.js
@@ -1,3 +1,12 @@
+// 要素オブジェクトの取得
+var getResult = document.getElementById('getResult');
+var result = document.getElementById('result');
+
+// イベントの登録
+getResult.addEventListener('click', function(){
+  result.innerHTML = '結果は「' + omikuji.getResult() + '」でした。';
+});
+
 // おみくじオブジェクトの定義
 var omikuji = {
   results: ["大吉","吉","中吉","小吉","凶"],
