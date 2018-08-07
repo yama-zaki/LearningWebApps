16ffd1e27abae25731e0b5fc7d66c5be346b8afb
yama-zaki <yamazaki@it-nasu.com>
2018-08-07 17:33:22 +0900

    メイン画像とサムネイルが表示できた

--- JavaScript学習/Brackets/sample_js/09/gallery/practice/js/app.js
+++ JavaScript学習/Brackets/sample_js/09/gallery/practice/js/app.js
@@ -13,8 +13,17 @@ mainImage.setAttribute('src', album[0].src);
 mainImage.setAttribute('alt', album[0].msg);
 
 var mainMsg = document.createElement('p');
-mainImage.innerText = mainImage.alt;
+mainMsg.innerText = mainImage.alt;
 
 var mainFlame = document.querySelector('#gallery .main');
 mainFlame.insertBefore(mainImage, null);
 mainFlame.insertBefore(mainMsg, null);
+
+// サムネイル写真画像の表示
+var thumbFlame = document.querySelector('#gallery .thumb');
+for (var i = 0; i < album.length; i++) {
+  var thumbImage = document.createElement('img');
+  thumbImage.setAttribute('src', album[i].src);
+  thumbImage.setAttribute('alt', album[i].msg);
+  thumbFlame.insertBefore(thumbImage, null);
+}
