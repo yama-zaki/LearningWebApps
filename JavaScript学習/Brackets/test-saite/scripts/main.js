8896134c414710b701223c3877aeb0b20fc2f132
yama-zaki <yamazaki@it-nasu.com>
2018-09-05 16:40:46 +0900

    画像を変更できるようにしました

--- JavaScript学習/Brackets/test-saite/scripts/main.js
+++ JavaScript学習/Brackets/test-saite/scripts/main.js
@@ -1,2 +1,12 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+  var mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-icon.png'){
+    myImage.setAttribute('src','images/nnchi4.jpg');
+  } else{
+    myImage.setAttribute('src','')
+  }
+   
+ 
+}
