d52af012195cb363c8147c94e96fec61014e535d
akm <akm2000@gmail.com>
2018-10-30 08:34:51 +0900

    :recycle: album, strings, linkを持つオブジェクトの配列recordsを定義しました。
    album, strings, linkはそれぞれ同じnumを使って参照される関連したデータ
    なので関連することをはっきりと明示するためにオブジェクトにまとめました。

--- JavaScript学習/Brackets/test-saite/scripts/main.js
+++ JavaScript学習/Brackets/test-saite/scripts/main.js
@@ -1,53 +1,51 @@
-var album = [
-  {src: 'images/Firefox.jpg', alt: 'My test image'},
-  {src: 'images/Nanachi.jpg', alt: 'My image'}
+var records = [
+  {
+    album: {src: 'images/Firefox.jpg', alt: 'My test image'},
+    strings: {
+      tM: 'My main title',
+      t1: 'My top level heading',
+      t2: 'My subheading',
+      t3: 'My sub-subheading',
+      m1: 'This is a single paragraph',
+      m2: 'At Mozilla, we’re a global community of',
+      m3: 'working togethr...',
+      l1: 'technologists',
+      l2: 'thinkers',
+      l3: 'builders'
+    },
+    link: {
+      url: 'https://www.mozilla.org/en-US/about/manifesto',
+      text: 'Mozilla Manifesto'
+    }
+  },
+  {
+    album: {src: 'images/Nanachi.jpg', alt: 'My image'},
+    strings: {
+      tM: 'ななちはかわいい',
+      t1: 'ミーティもかわいい',
+      t2: '疲れた時はかわいいを見よう',
+      t3: '和んだらお仕事に戻ろう',
+      m1: 'これは一つの物語です',
+      m2: 'このかわいいを作っているのは',
+      m3: 'ななちの画像をもっと見る？',
+      l1: 'ななち',
+      l2: 'ミーティ',
+      l3: 'なべ'
+    },
+    link: {
+      url: 'https://search.yahoo.co.jp/image/search?p=%E3%81%AA%E3%81%AA%E3%81%A1%E3%81%8C%E3%81%9E%E3%81%86&ei=UTF-8&fr=top_ga1_sa',
+      text: 'ななち画像へのリンク (Yahoo)'
+    }
+  }
 ];
 
 function toggleAlbumIndexOf(src) {
-  if (src === album[0].src) {
+  if (src === records[0].album.src) {
     return 1;
   }else{
     return 0;
   }
 }
-
-var strings = [
-  {
-    tM: 'My main title',
-    t1: 'My top level heading',
-    t2: 'My subheading',
-    t3: 'My sub-subheading',
-    m1: 'This is a single paragraph',
-    m2: 'At Mozilla, we’re a global community of',
-    m3: 'working togethr...',
-    l1: 'technologists',
-    l2: 'thinkers',
-    l3: 'builders'
-  },
-  {
-    tM: 'ななちはかわいい',
-    t1: 'ミーティもかわいい',
-    t2: '疲れた時はかわいいを見よう',
-    t3: '和んだらお仕事に戻ろう',
-    m1: 'これは一つの物語です',
-    m2: 'このかわいいを作っているのは',
-    m3: 'ななちの画像をもっと見る？',
-    l1: 'ななち',
-    l2: 'ミーティ',
-    l3: 'なべ'
-  }
-];
-
-var link = [
-  {
-    url: 'https://www.mozilla.org/en-US/about/manifesto',
-    text: 'Mozilla Manifesto'
-  },
-  {
-    url: 'https://search.yahoo.co.jp/image/search?p=%E3%81%AA%E3%81%AA%E3%81%A1%E3%81%8C%E3%81%9E%E3%81%86&ei=UTF-8&fr=top_ga1_sa',
-    text: 'ななち画像へのリンク (Yahoo)'
-  }
-];
 
 var myImage = document.querySelector('img');
 var mainTitle = document.querySelector('h1');
@@ -64,18 +62,22 @@ var lsti3 = document.getElementsByTagName('li')[2];
 
 myImage.onclick = function() {
   var num = toggleAlbumIndexOf(myImage.getAttribute('src'));
-  myImage.setAttribute('src', album[num].src);
-  myImage.setAttribute('alt', album[num].alt);
-  mainTitle.innerHTML = strings[num].tM;
-  subTitle1.innerHTML = strings[num].t1;
-  subTitle2.innerHTML = strings[num].t2;
-  subTitle3.innerHTML = strings[num].t3;
-  msgP1.innerHTML = strings[num].m1;
-  msgP2.innerHTML = strings[num].m2;
-  msgP3.innerHTML = strings[num].m3;
-  lsti1.innerHTML = strings[num].l1;
-  lsti2.innerHTML = strings[num].l2;
-  lsti3.innerHTML = strings[num].l3;
-  imgUrl.href = link[num].url;
-  imgUrl.innerText = link[num].text;
+  var record = records[num];
+  var album = record.album;
+  myImage.setAttribute('src', album.src);
+  myImage.setAttribute('alt', album.alt);
+  var strings = record.strings;
+  mainTitle.innerHTML = strings.tM;
+  subTitle1.innerHTML = strings.t1;
+  subTitle2.innerHTML = strings.t2;
+  subTitle3.innerHTML = strings.t3;
+  msgP1.innerHTML = strings.m1;
+  msgP2.innerHTML = strings.m2;
+  msgP3.innerHTML = strings.m3;
+  lsti1.innerHTML = strings.l1;
+  lsti2.innerHTML = strings.l2;
+  lsti3.innerHTML = strings.l3;
+  var link = record.link;
+  imgUrl.href = link.url;
+  imgUrl.innerText = link.text;
 }
