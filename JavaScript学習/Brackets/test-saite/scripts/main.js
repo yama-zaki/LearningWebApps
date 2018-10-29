f8b8c7536fe996ef93c35f6e6ecbb792c12a6df3
yama-zaki <yamazaki@it-nasu.com>
2018-10-29 13:42:43 +0900

    少しスッキリしてみました

--- JavaScript学習/Brackets/test-saite/scripts/main.js
+++ JavaScript学習/Brackets/test-saite/scripts/main.js
@@ -1,57 +1,66 @@
+var album = [
+  {src: 'images/Firefox.jpg', alt: 'My test image'},
+  {src: 'images/Nanachi.jpg', alt: 'My image'}
+];
+
+var strings = [{
+  tM: 'My main title',     t1: 'My top level heading', 
+  t2: 'My subheading',     t3: 'My sub-subheading', 
+  m1: 'This is a single paragraph', 
+  m2: 'At Mozilla, we’re a global community of', 
+  m3: 'working togethr...',
+  l1: 'technologists', l2: 'thinkers', l3: 'builders' 
+  }, {
+  tM: 'ななちはかわいい',         t1: 'ミーティもかわいい', 
+  t2: '疲れた時はかわいいを見よう', t3: '和んだらお仕事に戻ろう',
+  m1: 'これは一つの物語です',     
+  m2: 'このかわいいを作っているのは',
+  m3: 'ななちの画像をもっと見る？',
+  l1: 'ななち', l2: 'ミーティ', l3: 'なべ'    
+  } 
+];
+
+var link = [
+  {url: 'https://www.mozilla.org/en-US/about/manifesto', 
+   text: 'Mozilla Manifesto'},
+  {url: 'https://search.yahoo.co.jp/image/search?p=%E3%81%AA%E3%81%AA%E3%81%A1%E3%81%8C%E3%81%9E%E3%81%86&ei=UTF-8&fr=top_ga1_sa', text: 'ななち画像へのリンク (Yahoo)'}
+];
+
+function GetNumber(aStr) {
+  if (aStr === album[0].src) {
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
 var myImage = document.querySelector('img');
 var mainTitle = document.querySelector('h1');
 var subTitle1 = document.querySelector('h2');
 var subTitle2 = document.querySelector('h3');
 var subTitle3 = document.querySelector('h4');
 var imgUrl = document.querySelector('a');
-var msgP0 = document.getElementsByTagName('p')[0];
-var msgP1 = document.getElementsByTagName('p')[1];
-var msgP2 = document.getElementsByTagName('p')[2];
-var lsti0 = document.getElementsByTagName('li')[0];
-var lsti1 = document.getElementsByTagName('li')[1];
-var lsti2 = document.getElementsByTagName('li')[2];
-var bufm = mainTitle.innerHTML;
-var buf1 = subTitle1.innerHTML;
-var buf2 = subTitle2.innerHTML;
-var buf3 = subTitle3.innerHTML;
-var bufa = imgUrl.innerText;
-var bufu = imgUrl.href;
-var bufP0 = msgP0.innerHTML;
-var bufP1 = msgP1.innerHTML;
-var bufP2 = msgP2.innerHTML;
-var bufl0 = lsti0.innerHTML;
-var bufl1 = lsti1.innerHTML;
-var bufl2 = lsti2.innerHTML;
+var msgP1 = document.getElementsByTagName('p')[0];
+var msgP2 = document.getElementsByTagName('p')[1];
+var msgP3 = document.getElementsByTagName('p')[2];
+var lsti1 = document.getElementsByTagName('li')[0];
+var lsti2 = document.getElementsByTagName('li')[1];
+var lsti3 = document.getElementsByTagName('li')[2];
 
-myImage.onclick = function(){
-  var mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/Firefox.jpg'){
-    myImage.setAttribute('src','images/Nanachi.jpg');
-    mainTitle.innerHTML = 'ななちはかわいい';
-    subTitle1.innerHTML = 'ミーティもかわいい';
-    subTitle2.innerHTML = '疲れた時はかわいいを見よう';
-    subTitle3.innerHTML = '和んだらお仕事に戻ろう！';
-    imgUrl.href = "https://search.yahoo.co.jp/image/search?p=%E3%81%AA%E3%81%AA%E3%81%A1%E3%81%8C%E3%81%9E%E3%81%86&ei=UTF-8&fr=top_ga1_sa";
-    imgUrl.innerText = "ななち画像へのリンク (Yahoo)";
-    msgP0.innerHTML = 'これは一つの物語です';
-    msgP1.innerHTML = 'このかわいいを作っているのは...';
-    msgP2.innerHTML = 'ななちの画像をもっと見る？';
-    lsti0.innerHTML = 'ななち';
-    lsti1.innerHTML = 'ミーティ';
-    lsti2.innerHTML = 'なべ';
-  } else{
-    myImage.setAttribute('src','images/Firefox.jpg');
-    mainTitle.innerHTML = bufm;
-    subTitle1.innerHTML = buf1;
-    subTitle2.innerHTML = buf2
-    subTitle3.innerHTML = buf3;
-    imgUrl.href = bufu;
-    imgUrl.innerText = bufa;
-    msgP0.innerHTML = bufP0;
-    msgP1.innerHTML = bufP1;
-    msgP2.innerHTML = bufP2;
-    lsti0.innerHTML = bufl0;
-    lsti1.innerHTML = bufl1;
-    lsti2.innerHTML = bufl2; 
-  }  
-}
+myImage.onclick = function() {
+  var num = GetNumber(myImage.getAttribute('src'));
+  myImage.setAttribute('src', album[num].src);
+  myImage.setAttribute('alt', album[num].alt);
+  mainTitle.innerHTML = strings[num].tM;
+  subTitle1.innerHTML = strings[num].t1;
+  subTitle2.innerHTML = strings[num].t2;
+  subTitle3.innerHTML = strings[num].t3;
+  msgP1.innerHTML = strings[num].m1;
+  msgP2.innerHTML = strings[num].m2;  
+  msgP3.innerHTML = strings[num].m3;
+  lsti1.innerHTML = strings[num].l1;
+  lsti2.innerHTML = strings[num].l2;
+  lsti3.innerHTML = strings[num].l3;
+  imgUrl.href = link[num].url;
+  imgUrl.innerText = link[num].text;
+  }
