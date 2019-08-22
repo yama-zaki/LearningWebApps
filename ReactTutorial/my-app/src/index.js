4b55f89632e2f7151cd8b63593654548daa6d448
yama-zaki <yamazaki@it-nasu.com>
2019-08-22 11:18:11 +0900

    マスをクリックしたらXが表示されるようにしました

--- ReactTutorial/my-app/src/index.js
+++ ReactTutorial/my-app/src/index.js
@@ -11,7 +11,7 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
         {this.state.value}
       </button>
     );
