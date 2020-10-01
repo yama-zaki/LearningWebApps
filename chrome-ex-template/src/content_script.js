6545bf8356bb4dcaa791b28e397445f65a93a90b
yama-zaki <yamazaki@it-nasu.com>
2020-10-01 12:43:55 +0900

    ポップアップさせて選択するのを諦めました

--- chrome-ex-template/src/content_script.js
+++ chrome-ex-template/src/content_script.js
@@ -1,11 +1,27 @@
 ﻿//----------------------
-$(function () {
-    //↓疎通確認コード：タイトルの■が回転するはず
-    console.log('Chrome拡張の疎通確認コードが動いています。動作確認ができたら削除してください。');
-    let flag = false;
-    setInterval(function () {
-        document.title = (flag ? "■" : "◆") + document.title.replace(/^[■◆]/, '');
-        flag = !flag;
-    }, 1000);
-    //↑疎通確認コード
+$(function () {   
+    var urlParam = location.search.substring(1);
+    // URLにパラメータが存在する場合なにもしない
+    if(urlParam) {} else {
+      let url=location.href;
+      var result = url.search('...fix/');
+      if(result) {
+        location.replace(location.href + "?template=fix.md");
+      } else {
+        var result = url.search('...features/');
+        if(result) {
+          location.replace(location.href + "?template=features.md");
+        } else {
+          var result = url.search('...print/');
+          if(result) {
+            location.replace(location.href + "?template=print.md");
+          } else {
+            var result = url.search('...spec');
+            if(result) {
+              location.replace(location.href + "?template=spec.md");
+            }
+          }
+        }
+      }      
+    } 
 });
