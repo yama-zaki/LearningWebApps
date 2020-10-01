d35fdd9773f912955e53616235fa5d2f2619fb9d
yama-zaki <yamazaki@it-nasu.com>
2020-10-01 18:55:57 +0900

    スペースが入ってなかったので修正しました

--- chrome-ex-template/src/content_script.js
+++ chrome-ex-template/src/content_script.js
@@ -3,7 +3,7 @@ $(function () {
     var urlParam = location.search.substring(1);
     // URLにパラメータが存在する場合なにもしない
     if(urlParam) {} else {
-      let url=location.href;
+      let url = location.href;
       var result = url.match('...fix/');
       if(result) {
         location.replace(location.href + "?template=fix.md");
