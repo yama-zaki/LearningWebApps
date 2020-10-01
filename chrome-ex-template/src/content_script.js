a193fbdafb3cbbe76b2b8382c913922b03410616
yama-zaki <yamazaki@it-nasu.com>
2020-10-01 14:45:06 +0900

    うまく行く時もあるので一旦コミット

--- chrome-ex-template/src/content_script.js
+++ chrome-ex-template/src/content_script.js
@@ -4,24 +4,28 @@ $(function () {
     // URLにパラメータが存在する場合なにもしない
     if(urlParam) {} else {
       let url=location.href;
-      var result = url.search('...fix/');
+      var result = url.match('...fix/');
       if(result) {
         location.replace(location.href + "?template=fix.md");
+        location.reload;
       } else {
-        var result = url.search('...features/');
+        var result = url.match('...features/');
         if(result) {
           location.replace(location.href + "?template=features.md");
+          location.reload;
         } else {
-          var result = url.search('...print/');
+          var result = url.match('...print/');
           if(result) {
             location.replace(location.href + "?template=print.md");
+            location.reload;
           } else {
-            var result = url.search('...spec');
+            var result = url.match('...spec');
             if(result) {
               location.replace(location.href + "?template=spec.md");
-            }
-          }
-        }
-      }      
-    } 
+              location.reload;
+            };
+          };
+        };
+      };
+    };
 });
