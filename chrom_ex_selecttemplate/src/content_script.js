a405df60ce5080777324eea7782164bba98e1497
yama-zaki <yamazaki@it-nasu.com>
2020-10-02 17:32:54 +0900

    パラメータがある時に押されたボタン一緒なら何もしない、違うならパラメータを外して付け直すようにしました

--- chrom_ex_selecttemplate/src/content_script.js
+++ chrom_ex_selecttemplate/src/content_script.js
@@ -1,9 +1,25 @@
 ﻿//----------------------
 chrome.runtime.onMessage.addListener(function(msg) {
   console.log(msg)
-  var urlParam = location.search.substring(1);
+  let urlParam = location.search.substring(1);
+  let url;
   // URLにパラメータが存在し、同じ場合は何もしない
-  if(urlParam) {} else {
+  if(urlParam) {
+    let result = urlParam.match(msg.typename);
+    if(result) {} else {
+      url = location.href;
+      url = url.replace(urlParam, '');
+      if(msg.typename=='fix') {        
+        location.replace(url + "template=fix.md");
+      } else if(msg.typename=='features') {
+        location.replace(url + "template=features.md");
+      } else if(msg.typename=='print') {
+        location.replace(url + "template=print.md");
+      } else if(msg.typename=='spec') {
+        location.replace(url + "template=spec.md");
+      }; 
+    };
+  } else {
     if(msg.typename=='fix') {
       location.replace(location.href + "?template=fix.md");
     } else if(msg.typename=='features') {
