86582889c1169e9aa45a61659cdf13188af91bab
yama-zaki <yamazaki@it-nasu.com>
2020-10-02 14:20:55 +0900

    ボタンがクリックされたらメッセージを送るようにしました

--- chrom_ex_selecttemplate/src/popup.js
+++ chrom_ex_selecttemplate/src/popup.js
@@ -1,5 +1,31 @@
-$('.btn').on('click', function() {
-  chrome.runtime.sendMessage({ btnTxt: $(this).text() }, function(response) {
-    console.log(response)
-  })
-})
+$("#changefile_fix").on("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      typename: "fix"
+    });
+  });
+});
+
+$("#changefile_features").on("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      typename: "features"
+    });
+  });
+});
+
+$("#changefile_print").on("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      typename: "print"
+    });
+  });
+});
+
+$("#changefile_spec").on("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      typename: "spec"
+    });
+  });
+});
