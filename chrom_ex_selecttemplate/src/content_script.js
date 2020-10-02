ff7ce90e1607cf73457b52cc6446342671be577c
yama-zaki <yamazaki@it-nasu.com>
2020-10-02 14:21:21 +0900

    送られたメッセージに合わせてURLにテンプレのパラメータを追加するようにしました

---
+++ chrom_ex_selecttemplate/src/content_script.js
@@ -0,0 +1,17 @@
+﻿//----------------------
+chrome.runtime.onMessage.addListener(function(msg) {
+  console.log(msg)
+  var urlParam = location.search.substring(1);
+  // URLにパラメータが存在し、同じ場合は何もしない
+  if(urlParam) {} else {
+    if(msg.typename=='fix') {
+      location.replace(location.href + "?template=fix.md");
+    } else if(msg.typename=='features') {
+      location.replace(location.href + "?template=features.md");
+    } else if(msg.typename=='print') {
+      location.replace(location.href + "?template=print.md");
+    } else if(msg.typename=='spec') {
+      location.replace(location.href + "?template=spec.md");
+    };
+  };
+});
