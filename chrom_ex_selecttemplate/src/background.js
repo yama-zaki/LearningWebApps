67a09f135e4ebc70bb59d4cfa48ab707b1908c7a
yama-zaki <yamazaki@it-nasu.com>
2020-10-22 11:08:55 +0900

    フォルダ名を変更しました

--- chrom_ex_selecttemplate/src/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-  console.log(msg)
-  if (msg.btnTxt == 'hello!') {
-    sendResponse('Yeah!')
-  } else if (msg.btnTxt == 'good!') {
-    sendResponse('thank you!')
-  }
-})
